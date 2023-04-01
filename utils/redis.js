026b31e0b2e4e87e7614f2abb437885e343497c3
Chris Adebiyi <101924502+Chris-ade@users.noreply.github.com>
2023-04-01 13:38:30 +0100

    Update redis.js

--- utils/redis.js
+++ utils/redis.js
@@ -1,6 +1,12 @@
 const redis = require('redis');
 
+/**
+ * Represents a Redis client.
+ */
 class RedisClient {
+   /**
+   * Creates a new RedisClient instance.
+   */
   constructor() {
     this.client = redis.createClient();
 
@@ -9,6 +15,10 @@ class RedisClient {
     });
   }
 
+  /**
+   * Checks if this client's connection to the Redis server is active.
+   * @returns {boolean}
+   */
   async isAlive() {
     return new Promise((resolve) => {
       this.client.ping('PING', (err, result) => {
@@ -21,6 +31,11 @@ class RedisClient {
     });
   }
 
+  /**
+   * Retrieves the value of a given key.
+   * @param {String} key The key of the item to retrieve.
+   * @returns {String | Object}
+   */
   async get(key) {
     return new Promise((resolve, reject) => {
       this.client.get(key, (err, result) => {
@@ -33,6 +48,13 @@ class RedisClient {
     });
   }
 
+  /**
+   * Stores a key and its value along with an expiration time.
+   * @param {String} key The key of the item to store.
+   * @param {String | Number | Boolean} value The item to store.
+   * @param {Number} duration The expiration time of the item in seconds.
+   * @returns {Promise<void>}
+   */
   async set(key, value, duration) {
     return new Promise((resolve, reject) => {
       this.client.setex(key, duration, value, (err, result) => {
@@ -45,6 +67,11 @@ class RedisClient {
     });
   }
 
+  /**
+   * Removes the value of a given key.
+   * @param {String} key The key of the item to remove.
+   * @returns {Promise<void>}
+   */
   async del(key) {
     return new Promise((resolve, reject) => {
       this.client.del(key, (err, result) => {
@@ -59,5 +86,4 @@ class RedisClient {
 }
 
 const redisClient = new RedisClient();
-
 module.exports = redisClient;
