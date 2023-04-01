587ee7b71038bc48c27ec137c4b40203a8a6ade8
Chris Adebiyi <101924502+Chris-ade@users.noreply.github.com>
2023-04-01 14:08:54 +0100

    Update redis.js

--- utils/redis.js
+++ utils/redis.js
@@ -1,7 +1,13 @@
 const redis = require('redis');
 const { promisify } = require('util');
 
+/**
+ * Represents a Redis client.
+ */
 class RedisClient {
+  /**
+   * Creates a new RedisClient instance.
+   */
   constructor() {
     this.client = redis.createClient();
 
@@ -14,6 +20,10 @@ class RedisClient {
     this.delAsync = promisify(this.client.del).bind(this.client);
   }
 
+  /**
+   * Checks if this client's connection to the Redis server is active.
+   * @returns {boolean}
+   */
   async isAlive() {
     try {
       await this.client.pingAsync();
@@ -24,14 +34,31 @@ class RedisClient {
     }
   }
 
+  /**
+   * Retrieves the value of a given key.
+   * @param {String} key The key of the item to retrieve.
+   * @returns {String | Object}
+   */
   async get(key) {
     return this.getAsync(key);
   }
 
+  /**
+   * Stores a key and its value along with an expiration time.
+   * @param {String} key The key of the item to store.
+   * @param {String | Number | Boolean} value The item to store.
+   * @param {Number} duration The expiration time of the item in seconds.
+   * @returns {Promise<void>}
+   */
   async set(key, value, duration) {
     return this.setexAsync(key, duration, value);
   }
 
+  /**
+   * Removes the value of a given key.
+   * @param {String} key The key of the item to remove.
+   * @returns {Promise<void>}
+   */
   async del(key) {
     return this.delAsync(key);
   }
